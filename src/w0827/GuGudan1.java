e3cfa3cd57d8df385a7e8251be6618616bbde3ee
banbansolt <yoosanghoon3@gmail.com>
2025-08-27 10:36:06 +0900

    1차원배열에서 Java API에 있는 메소드들 사용(Arrays.copyOf(), sort()), enhanced for 사용

--- src/w0827/GuGudan1.java
+++ src/w0827/GuGudan1.java
@@ -1,6 +1,6 @@
-package W0827;
+package w0827;
 
-public class TEST {
+public class GuGudan1 {
     public static void main(String[] args) {
         for (int i=2; i <= 9; i++){
             for(int j=1; j <= 9; j++){
