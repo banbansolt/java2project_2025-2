303fa1f0085f24b6cd851a288c0fcfa2a71e46c5
banbansolt <yoosanghoon3@gmail.com>
2025-10-22 11:33:13 +0900

    s

--- src/w1022/ListAndComboTest.java
+++ src/w1022/ListAndComboTest.java
@@ -8,15 +8,13 @@ import java.awt.*;
 public class ListAndComboTest extends JFrame {
     JList list;
     JComboBox combo;
-    String[] items = {"사과","포도","딸기","오렌지","바나나"};
+    String[] items = {"사과", "토마토", "포도", "딸기", "오렌지", "바나나"};
     public ListAndComboTest(){
         setLayout(new FlowLayout());
-        list = new JList<>();
-        combo = new JComboBox();
+        list = new JList(items);
+        combo = new JComboBox(items);
         add(list);
         add(combo);
-
-
 
         setTitle("항목선택 가능 컴포넌트 JList and JComboBox");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
